3b024e5f9691ee0a91bdaf9ae50d6f34089236db
jerkald <jerkald@cc3850f6-5e36-474c-7b53-d56d12598e6b>
2012-01-28 21:32:30 +0000

    * image loading, logic class

--- actors/shape.py
+++ actors/shape.py
@@ -6,21 +6,32 @@ from pygame.sprite import Sprite
 import imageutils
 
 class Shape(Sprite):
+    loaded_images = {}
+
     def __init__(self, shape, color):
         Sprite.__init__(self) #call Sprite intializer
 
         self.color = color
         self.shape = shape
         self.texture_path = "%s_%s.png" % (color, self.shape)
-        self.image, self.rect = imageutils.load_image(self.texture_path, -1)
-        self.rect = self.image.get_rect(center=self.rect.center)
-        self.rect.move_ip((-self.image.get_width()/2, -self.image.get_height()/2))
-        self.small = pygame.transform.scale(self.image, (64,64))
+        self.image = pygame.transform.scale(self.getfile(self.texture_path), (64,64))
+        self.rect = self.image.get_rect();
+        self.original = self.image;
         
         self.rotation = 0;
 
     def update(self, delta):
         self.rotation = (self.rotation + 5) % 360
         center = self.rect.center
-        self.image = pygame.transform.rotate(self.small, self.rotation)
+        self.image = pygame.transform.rotate(self.original, self.rotation)
         self.rect = self.image.get_rect(center=center)
+        
+    def getfile(self, texture_path):
+        if texture_path in Shape.loaded_images:
+            return Shape.loaded_images[texture_path]
+        else:
+            image, rect = imageutils.load_image(texture_path, -1)
+            rect = image.get_rect(center=rect.center)
+            rect.move_ip((-image.get_width()/2, -image.get_height()/2))
+            Shape.loaded_images[texture_path] = image
+            return image
---
+++ logic.py
@@ -0,0 +1,29 @@
+import pygame
+
+class Logic:
+    def __init__(self, player, scoreboard, timer, control):
+        self.scoreboard = scoreboard
+        self.timer = timer
+        self.control = control
+        self.player = player
+    
+    def update(self, delta):
+        #Handle Input
+        if self.control.moveLeft:
+            self.player.rect.move_ip(-self.control.moverate, 0)
+        if self.control.moveRight:
+            self.player.rect.move_ip(self.control.moverate, 0)
+        if self.control.moveUp:
+            self.player.rect.move_ip(0, -self.control.moverate)
+        if self.control.moveDown:
+            self.player.rect.move_ip(0, self.control.moverate)       
+       
+        #Update Countdown Timer
+        self.timer.Milli -= delta;
+        self.timer.update()        
+
+        #Update score value
+        self.scoreboard.update()
+            
+        
+    
