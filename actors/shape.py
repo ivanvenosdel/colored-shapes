26ba87dedd01765bde8b5a02c46ad501cf7cec2b
imivan@gmail.com <imivan@gmail.com@cc3850f6-5e36-474c-7b53-d56d12598e6b>
2012-01-28 17:34:11 +0000

    Added module stubs for each new state. Filled out hexagon class

--- actors/shape.py
+++ actors/shape.py
@@ -6,11 +6,6 @@ import imageutils
 class Shape(Sprite):
     def __init__(self):
         Sprite.__init__(self) #call Sprite intializer
-        self.image, self.rect = imageutils.load_image('blue_octagon.png', -1)
-        self.rect.topleft = 80, 10
-
-
-
     
     def update(self):
         pass 
