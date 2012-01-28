015cd78646a73b7e9fd22703d544e44ee6a23ea5
imivan@gmail.com <imivan@gmail.com@cc3850f6-5e36-474c-7b53-d56d12598e6b>
2012-01-28 15:43:53 +0000

    Added some experimentation for movement

--- trunk/main.py
+++ trunk/main.py
@@ -22,17 +22,34 @@ class Shapescape:
         self.background = surface.Surface(self.screen.get_size()).convert()
         self.background.fill((0, 0, 0))
         
+        #Dudes
+        self.monster = pygame.image.load("monster.png").convert_alpha()
+        
         pygame.mouse.set_visible(1)
         
     # Continuesly renders the boid swarm
     def game_loop(self):
-        # Prepary to loop
         clock = pygame.time.Clock()
         do_continue = True
 
-        # Render the boid swarm
+        #TEMP
+        monster_x_position = 0
+        monster_y_position = 0
+        #END TEMP
         while do_continue:
             clock.tick(30) # fps
+            
+            #TEMP
+            monster_x_position += 5
+            if monster_x_position > self.win_width:
+                #Back to left
+                monster_x_position -= self.win_width
+                
+            monster_y_position += 5
+            if monster_y_position > self.win_height:
+                #Back to top
+                monster_y_position -= self.win_height
+            #END TEMP
 
             # Catch input event
             for event in pygame.event.get():
@@ -41,6 +58,7 @@ class Shapescape:
             
             # Update screen
             self.screen.blit(self.background, (0,0))
+            self.screen.blit(self.monster, (monster_x_position,monster_y_position))
             pygame.display.flip()        
         
         
