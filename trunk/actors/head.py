a4f151fed8e69884be2cb1e59ae2ca253c784af8
jerkald <jerkald@cc3850f6-5e36-474c-7b53-d56d12598e6b>
2012-01-29 19:54:40 +0000

    * some shit

--- trunk/actors/head.py
+++ trunk/actors/head.py
@@ -6,7 +6,7 @@ from actors.shape import Shape
 class Head(Shape):
     
     def __init__(self, size, rotation):
-        Shape.__init__(self, "head.png", size, rotation, "head")
+        Shape.__init__(self, "head.png", size, rotation, "head", "")
         
         self.sides = 1
         
--- trunk/actors/pentagon.py
+++ trunk/actors/pentagon.py
@@ -6,7 +6,7 @@ class Pentagon(Shape):
 
     def __init__(self, size, rotation, color="blue"):
         texture_path = Shape.build_texture_path(color, Pentagon.shape_type)
-        Shape.__init__(self, texture_path, size, rotation, Pentagon.shape_type)
+        Shape.__init__(self, texture_path, size, rotation, Pentagon.shape_type, color)
         
         self.sides = 5
         self.color = color
