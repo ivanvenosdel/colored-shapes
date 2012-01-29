9096655477c025209b259d96ee01794d0feec8a4
imivan@gmail.com <imivan@gmail.com@cc3850f6-5e36-474c-7b53-d56d12598e6b>
2012-01-29 04:12:48 +0000

    Finished simple player shape logic

--- actors/hexagon.py
+++ actors/hexagon.py
@@ -5,6 +5,9 @@ class Hexagon(Shape):
     shape_type = "hex"
 
     def __init__(self, size, rotation, color="blue"):
-        texture_path = Shape.build_texture_path(color, shape_type)
+        texture_path = Shape.build_texture_path(color, Hexagon.shape_type)
         Shape.__init__(self, texture_path, size, rotation)
+        
+        self.sides = 6
+        self.color = color
     
--- actors/tri.py
+++ actors/tri.py
@@ -5,6 +5,9 @@ class Tri(Shape):
     shape_type = "tri"
 
     def __init__(self, size, rotation, color="blue"):
-        texture_path = Shape.build_texture_path(color, shape_type)
+        texture_path = Shape.build_texture_path(color, Tri.shape_type)
         Shape.__init__(self, texture_path, size, rotation)
+        
+        self.sides = 3
+        self.color = color
     
